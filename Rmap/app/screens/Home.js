f2e6f1ffed131e56abdd8ab2522504003facb6e2
Drabblesaur <tojohnny1559@gmail.com>
2022-11-03 19:01:43 -0700

    Added Fonts + Text

--- Rmap/app/screens/Home.js
+++ Rmap/app/screens/Home.js
@@ -1,47 +1,61 @@
-import React, { useMemo, useRef } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {SafeAreaView, StyleSheet, Text, View, Image, Pressable} from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
+import {useFonts} from 'expo-font';
 
 function Home() {
-  const bottomSheetRef = React.useRef(null);
 
+  {/* Loading Fonts */}
+  const [fontsLoaded] = useFonts({
+    'Poppins-Regular' : require('./../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium' : require('./../assets/fonts/Poppins-Medium.ttf'),
+  });
+
+
+  {/* BottomSheet Consts */}
+  const bottomSheetRef = React.useRef(null);
   const snapPoints = useMemo(() => ['13%', '40%','95%'], []);
 
     return (
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container} >
         <StatusBar style="auto" />
         <BottomSheet
           ref={bottomSheetRef}
           index={1}
           snapPoints={snapPoints}
+          backgroundStyle={styles.bottomSheet}
         >
           <View style={styles.searchContainer}>
             {/* Search Bar */}
             <View style={styles.searchBox}>
-              <Text>Search</Text>
+              <Text style={styles.searchText}>Search</Text>
             </View>
             {/* Profile */}
             <Image style={styles.profileImage}/>
           </View>
-          <View style={styles.mainContainer}>
-            {/* Class & Directory Buttons*/}
-            <View style={styles.mainOptions}>
-              <Pressable style = {styles.classButton}>
+          <View style={styles.fullContainer}>
+            <View style={styles.mainContainer}>
+              {/* Class & Directory Buttons*/}
+              <View style={styles.mainOptions}>
+                <Pressable style = {styles.classButton}>
+                  <View style = {styles.classView}>
+                    <Text style={styles.pressableText}>Get me to{"\n"}class</Text>
+                  </View>
+                </Pressable>
+                <Pressable style = {styles.directoryButton}>
+                <Text style={styles.pressableText}>UCR{"\n"}Directory</Text>
+                </Pressable>
+              </View>
+              {/* Favorites & Settings Buttons*/}
+              <View style = {styles.secondOptions}>
+              <Pressable style = {styles.favoriteButton}>
 
               </Pressable>
-              <Pressable style = {styles.directoryButton}>
+              <Pressable style = {styles.settingButton}>
 
-              </Pressable>
-            </View>
-            {/* Favorites & Settings Buttons*/}
-            <View style = {styles.secondOptions}>
-            <Pressable style = {styles.smallPressable}>
-
-            </Pressable>
-            <Pressable style = {styles.smallPressable}>
-
-              </Pressable>
+                </Pressable>
+              </View>
             </View>
           </View>
         </BottomSheet>
@@ -51,8 +65,12 @@ function Home() {
 export default Home;
 
 const styles = StyleSheet.create({
+  bottomSheet:{
+    backgroundColor: '#F6F7F4',
+  },
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#D9D9D9',
     alignItems: 'center',
     justifyContent: 'center',
@@ -67,10 +85,15 @@ const styles = StyleSheet.create({
   searchBox: {
     height: 33,
     width: 300,
-    backgroundColor: '#F2F2F2',
+    backgroundColor: '#E7E7E7',
     borderRadius: 10,
     alignItems: 'start',
     justifyContent: 'center',
+  },
+  searchText: {
+    color: '#C1C1C1',
+    fontSize: 16,
+    fontFamily: 'Poppins-Regular',
   },
   profileImage: {
     height: 40,
@@ -83,6 +106,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+  fullContainer:{
+    width:'100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
   mainOptions: {
     height:210,
     width:302,
@@ -94,10 +123,20 @@ const styles = StyleSheet.create({
     justifyContent:'space-between',
   },
   classButton: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'end',
     height:210,
     width:146,
     backgroundColor: '#A6D49F',
     borderRadius: 10,
+  },
+  classView:{
+    backgroundColor: '#111111',
+    flexDirection: 'column',
+    alignItems: 'end',
+    width:123,
+    height: 50,
   },
   directoryButton: {
     height:210,
@@ -105,10 +144,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#478BFF',
     borderRadius: 10,
   },
-  smallPressable: {
+  pressableText:{
+    fontFamily : 'Poppins-Medium',
+    fontSize: 16,
+    color: '#FFFFFF',
+  },
+  favoriteButton: {
     height: 100,
     width: 64,
     borderRadius: 10,
-    backgroundColor: '#478BFF',
+    backgroundColor: '#FFB81C',
+  },
+  settingButton: {
+    height: 100,
+    width: 64,
+    borderRadius: 10,
+    backgroundColor: '#C1C1C1',
   }
 });
