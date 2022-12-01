2f1a56137106d291ab33b5a4feb802fcec179f58
jasmineojeda01 <57571541+jasmineojeda01@users.noreply.github.com>
2022-12-01 09:24:30 -0800

    Added few more buildings

--- Rmap/app/screens/MapViewer.js
+++ Rmap/app/screens/MapViewer.js
@@ -1,9 +1,9 @@
 import React, {Component, useState} from 'react';
-import { Button, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, SafeAreaView, Pressable } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import {BottomSheetBackdrop,BottomSheetBackdropProps,} from '@gorhom/bottom-sheet';
 import MapView, {Marker, Overlay} from 'react-native-maps';
-//import React, {Component, useState} from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
 const UCRCOORD1 = [33.973311, -117.328205];
 const UCRCOORD2 = [33.973378, -117.328108];
@@ -12,7 +12,13 @@ const SKYE_COORD1 = [33.974983757803216, -117.32906175571368];
 const SKYE_COORD2 = [33.9758205938672, -117.32864056005334];
 const SPROUL_COORD1 = [33.972357375536745, -117.33006897950474];
 const SPROUL_COORD2 = [33.973186960425124, -117.3294313360745];
+const WATKINS_COORD1 = [33.97223032141277, -117.32931341769752];
+const WATKINS_COORD2 = [33.973244835362586, -117.32855643846719];
+const CHASS_SOUTH_COORD1 = [33.974479656373006, -117.33084046063642];
+const CHASS_SOUTH_COORD2 = [33.97512051004066, -117.33025715007692];
 
+
+Icon.loadFont();
 function MapViewer(props){
   const { modalOpen } = props.route.params;
 
@@ -57,17 +63,26 @@ function MapViewer(props){
           /*customMapStyle={mapStyle}*/>
             
           {/* Building floor overlays
-              NOTE: May need to refactor, there are a lot of buildings on campus */}
+              NOTE: Need to refactor, there are a lot of buildings on campus */}
+          {/*
           <Overlay //Test, UCR logo
             image={require('../assets/UCR.png')}
             bounds={[UCRCOORD1, UCRCOORD2]}
             opacity={visibleFirst}
             hide={true}
           />
+          */}
           <Overlay // SKYE HALL
             image={require('../assets/skye.png')}
             bounds={[SKYE_COORD1, SKYE_COORD2]}
             opacity={visibleFirst}
+          />
+          <Marker 
+            coordinate={{
+              latitude: 33.97537507374277,
+              longitude: -117.32886487890542,
+            }}
+            title={'Skye Hall'}
           />
           <Overlay // SPROUL HALL, FIRST FLOOR
             image={require('../assets/sproul1.png')}
@@ -79,8 +94,28 @@ function MapViewer(props){
             bounds={[SPROUL_COORD1, SPROUL_COORD2]}
             opacity={visibleSecond}
           />
+          <Overlay // WATKINS HALL, FIRST FLOOR
+            image={require('../assets/watkins1.png')}
+            bounds={[WATKINS_COORD1, WATKINS_COORD2]}
+            opacity={visibleFirst}
+          />
+          <Overlay // WATKINS HALL, SECOND FLOOR
+            image={require('../assets/watkins2.png')}
+            bounds={[WATKINS_COORD1, WATKINS_COORD2]}
+            opacity={visibleSecond}
+          />
+          <Overlay // CHASS SOUHT, FIRST FLOOR
+            image={require('../assets/chasssouth1.png')}
+            bounds={[CHASS_SOUTH_COORD1, CHASS_SOUTH_COORD2]}
+            opacity={visibleFirst}
+          />
+          <Overlay // CHASS SOUHT, SECOND FLOOR
+            image={require('../assets/chasssouth2.png')}
+            bounds={[CHASS_SOUTH_COORD1, CHASS_SOUTH_COORD2]}
+            opacity={visibleSecond}
+          />
 
-          {/* A test marker*/}
+          {/* A test marker*/ /*
           <Marker 
             draggable
             coordinate={{
@@ -92,21 +127,26 @@ function MapViewer(props){
             }
             title={'Test Marker'}
             description={'This is a description of the marker'}
-          />
+          /> */
+          }
         </MapView>
 
         {/* Button container for toggling floor plans*/}
         <View style={ styles.buttonsContainer }>
-            <Button // FIRST FLOORS
+            <Pressable // FIRST FLOORS
               onPress={() => toggleOverlay(0)}
-              title="First"
-              color="#841584"
-            />
-            <Button // SECOND FLOORS
+              style={ styles.buttonsStyle }>
+              <View>
+                  <Text style={styles.pressableText}>1st</Text>
+              </View>
+            </Pressable>
+            <Pressable // SECOND FLOORS
               onPress={() => toggleOverlay(1)}
-              title="Second"
-              color="#841584"
-            />      
+              style={ styles.buttonsStyle }>
+              <View>
+                  <Text style={styles.pressableText}>2nd</Text>
+              </View>
+            </Pressable>
         </View>
           {/*<Button title ='go to Class' onPress={() => {props.navigation.navigate('Class');}}/>*/}
           <StatusBar/>
@@ -123,7 +163,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'flex-end',
   },
   mapStyle: {
@@ -135,11 +175,22 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     flexDirection: 'column',
-    //alignItems: 'flex-end',
-    //direction: 'rtl',
-    paddingVertical: 100,
-    paddingHorizontal: 0,
+    paddingVertical: 150,
+    alignItems: 'flex-end',
   },
+  buttonsStyle: {
+    backgroundColor: '#478BFF',
+    borderRadius: 10,
+  },
+  pressableText: {
+    fontFamily : 'PoppinsMedium',
+    fontSize: 18,
+    color: '#FFFFFF',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 2.5,
+    paddingBottom: 2.5,
+  }
 });
 
 export default MapViewer;
