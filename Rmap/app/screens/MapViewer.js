17ed3bef325bc685ac7e8ad34d140ebc1d39c341
jasmineojeda01 <57571541+jasmineojeda01@users.noreply.github.com>
2023-01-23 20:54:33 -0800

    Building toggle init

--- Rmap/app/screens/MapViewer.js
+++ Rmap/app/screens/MapViewer.js
@@ -2,26 +2,14 @@ import React, {Component, useState} from 'react';
 import { Button, StyleSheet, Text, View, Image, SafeAreaView, Pressable } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import {BottomSheetBackdrop,BottomSheetBackdropProps,} from '@gorhom/bottom-sheet';
-import MapView, {Marker, Overlay, Polygon, Geojson} from 'react-native-maps';
+import MapView, {Marker, Overlay, Polygon, Polyline, Circle, Geojson} from 'react-native-maps';
 import Icon from 'react-native-vector-icons/Feather';
+
 import Buildings from '../components/buildings.json';
-
-const UCRCOORD1 = [33.973311, -117.328205];
-const UCRCOORD2 = [33.973378, -117.328108];
-
-const SKYE_COORD1 = [33.974983757803216, -117.32906175571368];
-const SKYE_COORD2 = [33.9758205938672, -117.32864056005334];
-const SPROUL_COORD1 = [33.972357375536745, -117.33006897950474];
-const SPROUL_COORD2 = [33.973186960425124, -117.3294313360745];
-const WATKINS_COORD1 = [33.97223032141277, -117.32931341769752];
-const WATKINS_COORD2 = [33.973244835362586, -117.32855643846719];
-const CHASS_SOUTH_COORD1 = [33.974479656373006, -117.33084046063642];
-const CHASS_SOUTH_COORD2 = [33.97512051004066, -117.33025715007692];
-
+import BuildingsFirstFloors from '../components/buildingsFirst.json';
+const RADIUS = 1;
 
 Icon.loadFont();
-
-const buildings = Buildings;
 
 function MapViewer(props){
   const { modalOpen } = props.route.params;
@@ -32,25 +20,15 @@ function MapViewer(props){
     }
   }, [props.route.params?.modalOpen]);
 
-    // Visibility states per floor plan, will toggle the opacity to 1 or 0
-    const [visibleFirst, setVisibleFirst] = useState(0);
-    const [visibleSecond, setVisibleSecond] = useState(0);
-    // Arrays for floor plan visibility states
-    const visible = [visibleFirst, visibleSecond];
-    const setVisible = [setVisibleFirst, setVisibleSecond];
+    // Visibility states per floor plan
+    const [visibleBuilding, setBuilding] = useState(Buildings);
 
-    // Toggles floor plan on and off based on "floor" (index for floor plan visibility arrays)
+    // Toggle between floors
     const toggleOverlay = (floor) => {
-      if (visible[floor] == 0) {
-        setVisible[floor](1);
-      } else {
-        setVisible[floor](0);
-      }
-
-      for (var i = 0; i < visible.length; i++) {
-        if (i != floor) {
-          setVisible[i](0);
-        }
+      if (floor == 0) {
+        setBuilding(Buildings);
+      } else if (floor == 1) {
+        setBuilding(BuildingsFirstFloors);
       }
     };
   
@@ -66,81 +44,76 @@ function MapViewer(props){
           }}
           >
             
-          {
-            Buildings.features.map((b, i) =>
-              <Polygon
-                coordinates= {b.geometry.coordinates[0].map((x) => 
-                    ({latitude: x[1], longitude: x[0]}),
-                  )
-                }
-                key={`building-${i}`}
-                strokeColor="red"
-                fillColor="green"
-                strokeWidth={2}
-                tappable
-                onPress={() => console.log(`building-${i}`)}
-              />
+          {// Function to iterate and display current json dataset (visibleBuilding)
+            visibleBuilding.features.map((b) => {
+              //console.log(b.geometry.type);
+              if (b.geometry.type == "Polygon") {
+                return (
+                  <Polygon
+                    coordinates= {b.geometry.coordinates[0].map((x) => 
+                        ({latitude: x[1], longitude: x[0]}),
+                      )
+                    }
+                    key={`building-${b.id}`}
+                    strokeColor="red"
+                    
+                    strokeWidth={2}
+                    tappable
+                    onPress={() => console.log(`building-${b.id}`)}
+                  />
+                )
+              }
+              else if (b.geometry.type == "Point") {
+                return (
+                  <Circle
+                    center= {{
+                      latitude: b.geometry.coordinates[1], 
+                      longitude: b.geometry.coordinates[0]
+                    }
+                    }
+                    key={`classroom-${b.id}`}
+                    strokeColor="red"
+                    fillColor="blue"
+                    strokeWidth={2}
+                    radius = { RADIUS }
+                    tappable
+                    onPress={() => console.log(`classroom-${b.id}`)}
+                  />
+                )
+              }
+              else if (b.geometry.type == "LineString") {
+                return (
+                  <Polyline
+                      coordinates= {b.geometry.coordinates.map((x) => 
+                        ({latitude: x[1], longitude: x[0]}),
+                      )
+                    }
+                    key={`line-${b.id}`}
+                    strokeColor="red"
+                    strokeWidth={2}
+                  />
+                )
+              }
+              }
             )
           }
-          
-          <Overlay //Test, UCR logo
-            image={require('../assets/UCR.png')}
-            bounds={[UCRCOORD1, UCRCOORD2]}
-            opacity={visibleFirst}
-            hide={true}
-          />
-          
-          <Overlay // SKYE HALL
-            image={require('../assets/skye.png')}
-            bounds={[SKYE_COORD1, SKYE_COORD2]}
-            opacity={visibleFirst}
-          />
-          <Overlay // SPROUL HALL, FIRST FLOOR
-            image={require('../assets/sproul1.png')}
-            bounds={[SPROUL_COORD1, SPROUL_COORD2]}
-            opacity={visibleFirst}
-          />
-          <Overlay // SPROUL HALL, SECOND FLOOR
-            image={require('../assets/sproul2.png')}
-            bounds={[SPROUL_COORD1, SPROUL_COORD2]}
-            opacity={visibleSecond}
-          />
-          <Overlay // WATKINS HALL, FIRST FLOOR
-            image={require('../assets/watkins1.png')}
-            bounds={[WATKINS_COORD1, WATKINS_COORD2]}
-            opacity={visibleFirst}
-          />
-          <Overlay // WATKINS HALL, SECOND FLOOR
-            image={require('../assets/watkins2.png')}
-            bounds={[WATKINS_COORD1, WATKINS_COORD2]}
-            opacity={visibleSecond}
-          />
-          <Overlay // CHASS SOUHT, FIRST FLOOR
-            image={require('../assets/chasssouth1.png')}
-            bounds={[CHASS_SOUTH_COORD1, CHASS_SOUTH_COORD2]}
-            opacity={visibleFirst}
-          />
-          <Overlay // CHASS SOUHT, SECOND FLOOR
-            image={require('../assets/chasssouth2.png')}
-            bounds={[CHASS_SOUTH_COORD1, CHASS_SOUTH_COORD2]}
-            opacity={visibleSecond}
-          />
+
         </MapView>
 
         {/* Button container for toggling floor plans*/}
         <View style={ styles.buttonsContainer }>
-            <Pressable // FIRST FLOORS
+            <Pressable // BUILDINGS
               onPress={() => toggleOverlay(0)}
               style={ styles.buttonsStyle }>
               <View>
-                  <Text style={styles.pressableText}>1st</Text>
+                  <Text style={styles.pressableText}>Buildings</Text>
               </View>
             </Pressable>
-            <Pressable // SECOND FLOORS
+            <Pressable // FIRST FLOORS
               onPress={() => toggleOverlay(1)}
               style={ styles.buttonsStyle }>
               <View>
-                  <Text style={styles.pressableText}>2nd</Text>
+                  <Text style={styles.pressableText}>First</Text>
               </View>
             </Pressable>
         </View>
