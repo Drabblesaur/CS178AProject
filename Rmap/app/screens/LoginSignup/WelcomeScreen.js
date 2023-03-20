f3e6c3bec7e35e47d94924353ff419f61483762f
Nick Lin <nickyhlin@gmail.com>
2023-03-19 22:57:52 -0700

    Testing
    CSS adding for login

--- Rmap/app/screens/LoginSignup/WelcomeScreen.js
+++ Rmap/app/screens/LoginSignup/WelcomeScreen.js
@@ -3,6 +3,7 @@ import {View, Button,StyleSheet,Text } from "react-native";
 function WelcomeScreen(props) {
     return (
         <View style={styles.container}>
+        <Image style={styles.tinylogo} source={require('./adaptive-icon.png')}></Image>
         <Text style={styles.text}>Get to your classes faster</Text>
         <Button title ='Sign Up' onPress={() => {
           props.navigation.navigate('Signup');
@@ -17,5 +18,17 @@ function WelcomeScreen(props) {
 export default WelcomeScreen;
 
 const styles = StyleSheet.create({
-    container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+    container:{ 
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      backgroundColor: '003DA5',
+    },
+
+    tinylogo:{
+      width: 50,
+      height: 50,
+      justifyContent: 'center',
+      alignItems:'center',
+    }
 });
