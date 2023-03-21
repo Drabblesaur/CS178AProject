8e8b5b225e1171e7272901ef2ebf29bdf4f0a160
Nick Lin <nickyhlin@gmail.com>
2023-03-21 03:49:01 -0700

    Completion CSS
    Finishing the CSS for login page

--- Rmap/app/screens/LoginSignup/Signup.js
+++ Rmap/app/screens/LoginSignup/Signup.js
@@ -1,10 +1,6 @@
 
-import {View, Button,StyleSheet,Text,TextInput, ActivityIndicator } from "react-native";
+import {View, Button,StyleSheet,Text,TextInput, TouchableOpacity,SafeAreaView, Image } from "react-native";
 import React, { useState } from 'react';
-
-
-
-
 
 
 const Signup = (props) => {
@@ -50,44 +46,130 @@ const Signup = (props) => {
   }
 }
         
-       
-
-
-   
+         
   
     return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={setPassword}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Confirm Password"
-          secureTextEntry={true}
-          value={confirmpassword}
-          onChangeText={setconfirmpassword}
-        />
-        <Button title="Register" onPress={registerHandler} />
-      </View>
+      <SafeAreaView style={styles.safe_container}>
+        <View style={styles.container}>
+        <Image style = {styles.logo} source={require('../../assets/Rmap.png')}></Image>
+        <Text style = {styles.signup_text}>Sign Up</Text>
+        <View style ={styles.input_container}>
+          <Text style = {styles.text}>Email:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="example@ucr.edu"
+              placeholderTextColor="#BCBCBC"
+              value={email}
+              onChangeText={setEmail}
+            />
+        </View>
+        <View style ={styles.input_container}>
+        <Text style = {styles.text}>Password:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="8+ Characters"
+            placeholderTextColor="#BCBCBC"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        <View style ={styles.input_container}>
+          <Text style = {styles.text}>Confirm Password:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Confirm Password"
+            placeholderTextColor="#BCBCBC"
+            secureTextEntry={true}
+            value={confirmpassword}
+            onChangeText={setconfirmpassword}
+          />
+          </View>
+          <TouchableOpacity style={styles.account_container} onPress={registerHandler}>
+            <View style={styles.button_text}>
+              <Text style = {styles.buttonText}>Create an Account</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     );
   };
 
   
 
-
-
 export default Signup;
 
 const styles = StyleSheet.create({
-    container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-});
+
+  safe_container:{
+    flex:1,
+    backgroundColor: '#2C4EDC',
+  },
+  
+  container:{ 
+    flexDirection: 'column',
+    alignItems: 'center', 
+    justifyContent: 'center',
+    backgroundColor: '#2C4EDC',
+  },
+  input_container:{
+    //backgroundColor:'red',
+    width:'80%',
+    alignItems:'start',
+    marginBottom:20
+  },
+  input:{
+    fontSize: 18,
+    width: '100%',
+    justifyContent:'center',
+    borderRadius:10,
+    padding: 10,
+    backgroundColor: '#E7E7E7'
+  },
+
+  signup_text:{
+    color: '#FAF4EE',
+    fontSize: 36,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  
+  text:{
+    marginTop:20,
+    fontSize: 20, 
+   // alignItems: 'center', 
+    color: "#FFF",
+    textAlign: 'right',
+    marginBottom:10,
+    fontWeight: "bold",
+  },
+
+  account_container:{
+    marginTop:30,
+    backgroundColor:'#478BFF',
+    width:'60%',
+    minHeight:60,
+    justifyContent:'center',
+    borderRadius:20
+  },
+
+  buttonText:{
+    fontSize: 20, 
+    fontWeight: "bold",
+    //alignSelf: "center ",
+    color: "#FFF",
+  },
+
+  button_text:{
+    //backgroundColor :"black",
+    width: "100%",
+    alignItems: 'center'
+  },
+
+  logo:{
+    height:46,
+    width:135,
+    marginTop: 35,
+  },
+})
--- Rmap/app/screens/LoginSignup/WelcomeScreen.js
+++ Rmap/app/screens/LoginSignup/WelcomeScreen.js
@@ -1,34 +1,106 @@
-import {View, Button,StyleSheet,Text } from "react-native";
+import {View, Button,StyleSheet,Text,Image, TouchableOpacity,SafeAreaView } from "react-native";
 
 function WelcomeScreen(props) {
     return (
-        <View style={styles.container}>
-        <Image style={styles.tinylogo} source={require('./adaptive-icon.png')}></Image>
-        <Text style={styles.text}>Get to your classes faster</Text>
-        <Button title ='Sign Up' onPress={() => {
-          props.navigation.navigate('Signup');
-        }}/>
-        <Button title ='I have an account' onPress={() => {
-          props.navigation.navigate('LoginScreen');
-        }}/>
+      <SafeAreaView style={styles.safe_container}>
+          <View style={styles.container}>
+          <Image style = {styles.logo} source={require('../../assets/Rmap.png')}></Image>
+          <Text style={styles.text}>Get to your{"\n"} classes faster</Text>
+          <Image source={require('../../assets/main_1.png')}></Image>
+          <TouchableOpacity style ={styles.signup_container} onPress={() => {props.navigation.navigate('Signup')}}>
+            <View style={styles.button_text}>
+              <Text style = {styles.signup_Text}>Sign up</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.account_container} onPress={() => {props.navigation.navigate('LoginScreen')}}>
+            <View style={styles.button_text}>
+              <Text style = {styles.buttonText}>I Have an Account</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-    );
+      </SafeAreaView>
+  );
 }
+//<Image stlye = {styles.logo} source={require('../../assets/Rmap.png')}></Image>
+//<Image source={require('../../assets/main_1.png')}></Image>
+//<Image source={require('../../assets/asset-1.svg')}></Image>
 
 export default WelcomeScreen;
 
 const styles = StyleSheet.create({
+    safe_container:{
+      flex:1,
+      backgroundColor: '#2C4EDC',
+    },
     container:{ 
-      flex: 1, 
+      flexDirection: 'column',
       alignItems: 'center', 
       justifyContent: 'center',
-      backgroundColor: '003DA5',
+      backgroundColor: '#2C4EDC',
     },
 
-    tinylogo:{
-      width: 50,
-      height: 50,
-      justifyContent: 'center',
-      alignItems:'center',
+    logo:{
+      height:46,
+      width:135,
+      marginTop: 35,
+    },
+   
+    text:{
+      marginTop:20,
+      fontSize: 36, 
+      fontWeight: "bold",
+      alignItems: 'center', 
+      color: "#FFF",
+      textAlign: 'center',
+      marginBottom:30
+    },
+
+    signup_container:{
+      marginTop:30,
+      backgroundColor:'#478BFF',
+      width:'70%',
+      minHeight:60,
+      justifyContent:'center',
+      borderRadius:10
+    },
+    button_text:{
+      //backgroundColor :"black",
+      width: "100%",
+      alignItems: 'center'
+    },
+    
+    buttonText:{
+      fontSize: 20, 
+      fontWeight: "bold",
+      //alignSelf: "center ",
+      color: "#478BFF",
+    },
+    
+    signup_Text:{
+      fontSize: 20, 
+      fontWeight: "bold",
+      //alignSelf: "center ",
+      color: "#FFF",
+    },
+
+    account_container:{
+      marginTop:20,
+      backgroundColor:'white',
+      width:'70%',
+      minHeight:60,
+      justifyContent:'center',
+      borderRadius:10
+    },
+    
+    account_button:{
+      backgroundColor: "#F6F7F9",
+      borderRadius: 10,
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+      alignItems: 'center',
+      position: 'absolute',
+      paddingLeft:38,
+      paddingRight:38,
+      textAlign: 'center',
     }
 });
